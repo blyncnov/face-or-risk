348e8676ddfdb5b324247644c98beebe1b0a611c
blyncnov <bholuwatife00@gmail.com>
2023-12-31 12:19:05 +0100

    Face or Risk: Choose cards logic

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,23 +1,88 @@
 "use client";
 
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { useSearchParams } from "next/navigation";
 
 // components
 import FaceOrRiskCard from "@/components/Card";
 
+const GameDares = [
+  {
+    id: 1,
+    dare: "1. Everyone who is still in contact with their ex takes a shot.",
+  },
+  {
+    id: 2,
+    dare: "2. Everyone who is still in contact with their ex takes a shot.",
+  },
+  {
+    id: 3,
+    dare: "3. Everyone who is still in contact with their ex takes a shot.",
+  },
+  {
+    id: 4,
+    dare: "4. Everyone who is still in contact with their ex takes a shot.",
+  },
+  {
+    id: 5,
+    dare: "5. Everyone who is still in contact with their ex takes a shot.",
+  },
+  {
+    id: 6,
+    dare: "6. Everyone who is still in contact with their ex takes a shot.",
+  },
+];
+
 const GameRoom = () => {
   const searchParams = useSearchParams();
+  const StackCardRef = useRef<HTMLDivElement | any>(null);
+  const cardRef = useRef<HTMLDivElement>(null);
+
+  const [GameCard, setGameCard] =
+    useState<{ id: number; dare: string }[]>(GameDares);
+
   const gameID = searchParams.get("id");
 
-  // logger
-  console.log(gameID);
+  const ChooseCard = (e: any) => {
+    const lastChildElement = StackCardRef.current?.lastChild;
+    console.log(e.target === lastChildElement);
+
+    if (cardRef.current && cardRef.current.style) {
+      if (e.target !== lastChildElement) return;
+
+      const style = cardRef.current.style;
+
+      if (style) {
+        // Add card Animation
+        style.animation = "swap 700ms forwards";
+      }
+
+      setTimeout(() => {
+        // Add card Animation
+
+        if (style) {
+          // Add card Animation
+          style.animation = "";
+        }
+
+        // Prepend card
+        StackCardRef.current?.prepend(lastChildElement);
+      }, 700);
+    }
+  };
 
   return (
     <div className="page-not-found w-full min-h-screen flex flex-col gap-6 justify-center items-center text-center">
-      <div className="stack_cards">
-        {[1, 2, 3, 4, 5, 6].map((card) => {
-          return <FaceOrRiskCard key={card} card={card} />;
+      <div ref={StackCardRef} className="stack_cards" onClick={ChooseCard}>
+        {GameCard.map((card: { id: number; dare: string }) => {
+          return (
+            <FaceOrRiskCard
+              key={card.id}
+              card={card}
+              click={ChooseCard}
+              cardRef={cardRef}
+            />
+          );
         })}
       </div>
     </div>
@@ -25,3 +90,20 @@ const GameRoom = () => {
 };
 
 export default GameRoom;
+
+// let stack = document.querySelector(".stack"); DONE
+
+// [...stack.children].reverse().forEach((i) => stack.append(i));
+
+// stack.addEventListener("click", swap);
+
+// function swap(e) {
+//   let card = document.querySelector(".card:last-child");
+//   if (e.target !== card) return;
+//   card.style.animation = "swap 700ms forwards";
+
+//   setTimeout(() => {
+//     card.style.animation = "";
+//     stack.prepend(card);
+//   }, 700);
+// }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 
-const FaceOrRiskCard = ({ card }: { card: number }) => {
+const FaceOrRiskCard = ({
+  card,
+  click,
+  cardRef,
+}: {
+  card: any;
+  click: any;
+  cardRef: any;
+}) => {
   return (
-    <div className="card w-full h-[400px] max-w-[250px] min-w-[250px] min-h-[400px] max-h-[400px] text-[var(--primary-color)] bg-white rounded-lg p-2 shadow cursor-move">
+    <div
+      onClick={click}
+      ref={cardRef}
+      className="card w-full h-[400px] max-w-[250px] min-w-[250px] min-h-[400px] max-h-[400px] text-[var(--primary-color)] bg-white rounded-lg p-2 shadow cursor-move"
+    >
       <div className="w-full h-full bg-white border border-[#f63f3f7c] rounded-lg p-3 grid grid-rows-[1fr_1fr_auto]">
         <div className="w-full">
           <h1 className="page-title text-5xl face_or_risk_font text-[var(--secondary-color)]">
@@ -11,11 +23,11 @@ const FaceOrRiskCard = ({ card }: { card: number }) => {
         </div>
         <div className="w-full text-black">
           <h1 style={{ lineHeight: 1.5 }} className="text-lg">
-            Everyone who is still in contact with their ex takes a shot.
+            {card.dare}
           </h1>
         </div>
         <div className="w-full text-black text-start">
-          <h1 className="text-2xl">{`#${card}`}</h1>
+          <h1 className="text-2xl">{`#${card.id}`}</h1>
         </div>
       </div>
     </div>
@@ -23,20 +35,3 @@ const FaceOrRiskCard = ({ card }: { card: number }) => {
 };
 
 export default FaceOrRiskCard;
-
-// let stack = document.querySelector(".stack");
-
-// [...stack.children].reverse().forEach((i) => stack.append(i));
-
-// stack.addEventListener("click", swap);
-
-// function swap(e) {
-//   let card = document.querySelector(".card:last-child");
-//   if (e.target !== card) return;
-//   card.style.animation = "swap 700ms forwards";
-
-//   setTimeout(() => {
-//     card.style.animation = "";
-//     stack.prepend(card);
-//   }, 700);
-// }
